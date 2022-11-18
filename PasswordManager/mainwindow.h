b42aae89c2a99288f35bc12e8ab4af0f95fb2858
ArcoKostyleff <104649358+ArcoKostyleff@users.noreply.github.com>
2022-11-18 12:38:35 +0300

    Add window change

--- PasswordManager/mainwindow.h
+++ PasswordManager/mainwindow.h
@@ -7,6 +7,7 @@
 #include <QString>
 #include "QtSql/QSqlDatabase"
 #include "QSqlQuery"
+#include "LoginForm.h"
 namespace Ui {
 class MainWindow;
 }
@@ -24,6 +25,7 @@ private slots:
     void on_login_Button_clicked();
 
 private:
+     LoginForm *m_login_form_ptr;
      QSqlDatabase db;
      QMap<QString, QString> m_users_map;
     Ui::MainWindow *ui;
